b4929b847218b2b6d4f5a8b72b698301b9282863
Wahyudi-1 <147951968+Wahyudi-1@users.noreply.github.com>
2025-08-24 08:17:34 +0700

    Create site.config.js

---
+++ site.config.js
@@ -0,0 +1,5 @@
+// File: site.config.js
+// KONFIGURASI UNTUK SITUS SEKOLAH A
+
+// PENTING: Ganti dengan UUID asli dari tabel 'sekolah' untuk sekolah ini.
+export const TARGET_SEKOLAH_ID = '221c8451-5951-4432-8e19-8c57fd9acd97';
